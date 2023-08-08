3dcddf8d792a974c0cc4d0a2037cf160dcb2d7ab
Ludmila Kurochkina <ludmilka.kurochkina@gmail.com>
2023-08-08 18:33:53 +0300

    Add AppBar

--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -1,5 +1,17 @@
+import { Navigation } from '../Navigation';
+import { UserMenu } from '../UserMenu';
+import { AuthNav } from '../AuthNav';
+import { useAuth } from 'hooks';
+import css from './AppBar.module.css';
+
 export const AppBar = () => {
+  const { isLoggedIn } = useAuth();
   return (
-    <></>
+    <>
+      <header className={css.header}>
+        <Navigation />
+        {isLoggedIn ? <UserMenu /> : <AuthNav />}
+      </header>
+    </>
   );
 };
